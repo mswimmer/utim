639bc0f3c254e2131c102f880e33fc1856b673c7
Morton Swimmer <morton_swimmer@trendmicro.com>
2019-07-11 15:30:32 +0200

    refactor some of the conversion code to speed things up

--- attack/attack2rdf.py
+++ attack/attack2rdf.py
@@ -14,63 +14,97 @@ def read_file(filename):
     with open(filename, 'r') as f:
         return json.loads(f.read())
 
+missed = {}
+missed_e = {}
+
 ##### ELEMENTS #####
 
-def do_default_e(n, s, o):
-    g = Graph()
-    return g
+def do_default_e(n, g, s, o):
+    pass
 
-def do_s_p_l(n, s, p, o):
-    g = Graph()
+def do_s_p_l(n, g, s, p, o):
     g.add( (s, p, Literal(o)) )
-    return g
 
-def do_s_p_lt(n, s, p, o, t):
-    g = Graph()
+def do_s_p_lt(n, g, s, p, o, t):
     g.add( (s, p, Literal(o, datatype=t)) )
-    return g
 
-def do_aliases(n, s, o):
-    g = Graph()
+def do_aliases(n, g, s, o):
     for i in o:
         g.add( (s, DCTERMS.alternative, Literal(i)) )
-    return g
 
-def do_kill_chain_phase(n, s, o):
-    g = Graph()
+def do_kill_chain_phase(n, g, s, o):
     for i in o:
         g.add( (s, n['killChainPhase'], n[i["kill_chain_name"]+'__'+i["phase_name"]]) )
-    return g
 
-def do_tactic_refs(n, s, o):
-    g = Graph()
+def do_tactic_refs(n, g, s, o):
+    """
+    This only seems to exist once in the file and is used like this:
+    {
+      "type": "x-mitre-matrix",
+      "id": "x-mitre-matrix--eafc1b4c-5e56-4965-bd4e-66a6a89c88cc",
+      "created_by_ref": "identity--c78cb6e5-0c4b-4611-8297-d1b8b55e40b5",
+      "created": "2018-10-17T00:14:20.652Z",
+      "modified": "2018-11-06T19:05:34.143Z",
+      "name": "Enterprise ATT&CK",
+      "description": "The full ATT&CK Matrix ... ",
+      "tactic_refs": [
+        "x-mitre-tactic--ffd5bcee-6e16-4dd2-8eca-7b3beedf33ca",
+        "x-mitre-tactic--4ca45d45-df4d-4613-8980-bac22d278fa5",
+        ...
+        ]
+    ...
+    """
     for i in o:
         refnode = n[i]
         g.add( (s, CTI.tactic, refnode) )
         g.add( (refnode, RDF.type, CTI.Tactic ) )
-    return g
-    
-def do_external_references(n, s, o):
-    g = Graph()
-    for i in o:
-        m = hashlib.sha256()
-        for k in sorted(i.keys()):
-            m.update((k+i[k]).encode('utf-8'))
-        refnode = n['ref_'+m.hexdigest()]
-        g.add( (s, CORE['reference'], refnode) )
-        g.add( (refnode, RDF.type, CORE.Reference) )
-        if "url" in i:
-            g.add( (refnode, CORE.referenceURL, Literal(i['url'], datatype=XSD.anyURI)) )
-        if "source_name" in i:
-            g.add( (refnode, CORE.referenceSource, Literal(i['source_name'], datatype=XSD.token)) )
-        if "external_id" in i:
-            g.add( (refnode, CORE.externalID, Literal(i['external_id'], datatype=XSD.token)) )
-        if "description" in i:
-            g.add( (refnode, DCTERMS.description, Literal(i['description'])) )
-    return g
 
-def do_x_mitre_permissions_required(n, s, o):
-    g = Graph()
+def do_external_reference(n, g, s, o, i):
+    """
+    looks like this:         
+    {
+      "url": "https://...",
+      "source_name": "...",
+      "external_id": "...",
+      "description": "..."
+    }
+    """
+    m = hashlib.sha256()
+    for k in sorted(i.keys()):
+        m.update((k+i[k]).encode('utf-8'))
+    refnode = n['ref_'+m.hexdigest()]
+    g.add( (s, CORE['reference'], refnode) )
+    g.add( (refnode, RDF.type, CORE.Reference) )
+    if "url" in i:
+        g.add( (refnode, CORE.referenceURL, Literal(i['url'], datatype=XSD.anyURI)) )
+    if "source_name" in i:
+        g.add( (refnode, CORE.referenceSource, Literal(i['source_name'], datatype=XSD.token)) )
+    if "external_id" in i:
+        g.add( (refnode, CORE.externalID, Literal(i['external_id'], datatype=XSD.token)) )
+    if "description" in i:
+        g.add( (refnode, DCTERMS.description, Literal(i['description'])) )
+    
+def do_external_references(n, g, s, o):
+    """
+    Could look like this:
+    "external_references": [
+        { ... }, ...
+      ],
+    """
+    for i in o:
+        do_external_reference(n, g, s, o, i)
+
+def do_x_mitre_permissions_required(n, g, s, o):
+    """
+      "x_mitre_permissions_required": [
+        "Administrator",
+        "SYSTEM"
+      ],
+    Out of this we create something like:
+    attack:s score:hasPrivilegesRequired attack:Administrstor .
+    attack:Administrstor a score:CVSSv3HighPrivilegesRequired .
+    ...
+    """
     for i in o:
         refnode = n[urllib.parse.quote_plus(i)]
         g.add( (s, SCORE['hasPrivilegesRequired'], refnode) )
@@ -80,106 +114,114 @@ def do_x_mitre_permissions_required(n, s, o):
             g.add( (refnode, RDF.type, SCORE.CVSSv3HighPrivilegesRequired) )
         else:
             print("WARNING: unknown privileges type '{}'".format(i))
-    return g
-
-def do_created_by_ref(n, s, o):
-    g = Graph()
+            
+def do_created_by_ref(n, g, s, o):
+    """
+    Like:
+      "created_by_ref": "identity--c78cb6e5-0c4b-4611-8297-d1b8b55e40b5",
+    """
     refnode = n[o]
     g.add( (s, DCTERMS['creator'], refnode) )
     g.add( (refnode, RDF.type, DCTERMS.Agent) )
-    return g
 
-def do_x_mitre_platforms(n, s, o):
-    g = Graph()
+def do_x_mitre_platforms(n, g, s, o):
+    """
+    Like:
+      "x_mitre_platforms": [
+        "Linux",
+        "macOS",
+        "Windows"
+      ],
+    """
     for i in o:
         refnode = n[i]
         g.add( (s, PLATFORM['platform'], refnode) )
         g.add( (refnode, RDF.type, CORE.Platform) )
-    return g
 
-def do_object_marking_refs(n, s, o):
-    g = Graph()
+def do_object_marking_refs(n, g, s, o):
+    """
+      "object_marking_refs": [
+        "marking-definition--fa42a846-8d90-4e51-bc29-71d5b4802168"
+      ],
+    """
     for i in o:
         refnode = n[i]
         g.add( (s, DCTERMS.rights, refnode) )
         g.add( (refnode, RDF.type, DCTERMS.RightsStatement) )
-    return g
 
-def process_element(n, s, o, exclusion=['id', 'type']):
-    g = Graph()
-    for k in o.keys():
-        if not k in exclusion:
-            if k in xe:
-                g += xe[k](n, s, o[k])
-            else:
-                print("WARNING: Don't know how to process data element '{}'".format(k))
-    return g
-
-xe = {
-    "created_by_ref":               lambda n, s, o: do_created_by_ref(n, s, o),
-    "name":                         lambda n, s, o: do_s_p_l(n, s, DCTERMS.title, o),
-    "description":                  lambda n, s, o: do_s_p_l(n, s, DCTERMS.description, o),
-    "external_references":          lambda n, s, o: do_external_references(n, s, o),
-    "object_marking_refs":          lambda n, s, o: do_object_marking_refs(n, s, o),
-    "x_mitre_version":              lambda n, s, o: do_default_e(n, s, o),
-    "x_mitre_data_sources":         lambda n, s, o: do_default_e(n, s, o),
-    "x_mitre_detection":            lambda n, s, o: do_default_e(n, s, o),
-    "x_mitre_permissions_required": lambda n, s, o: do_x_mitre_permissions_required(n, s, o),
-    "x_mitre_platforms":            lambda n, s, o: do_x_mitre_platforms(n, s, o),
-    "x_mitre_contributors":         lambda n, s, o: do_default_e(n, s, o),
-    "x_mitre_effective_permissions":lambda n, s, o: do_default_e(n, s, o),
-    "x_mitre_system_requirements":  lambda n, s, o: do_default_e(n, s, o),
-    "x_mitre_remote_support":       lambda n, s, o: do_default_e(n, s, o),
-    "x_mitre_network_requirements": lambda n, s, o: do_default_e(n, s, o),
-    "x_mitre_defense_bypassed":     lambda n, s, o: do_default_e(n, s, o),
-    "kill_chain_phases":            lambda n, s, o: do_kill_chain_phase(n, s, o),
-    "modified":                     lambda n, s, o: do_s_p_lt(n, s, DCTERMS.modifed, o, XSD.dateTime),
-    "created":                      lambda n, s, o: do_s_p_lt(n, s, DCTERMS.created, o, XSD.dateTime),
-    "source_ref":                   lambda n, s, o: do_default_e(n, s, o),
-    'relationship_type':            lambda n, s, o: do_default_e(n, s, o),
-    "target_ref":                   lambda n, s, o: do_default_e(n, s, o),
-    "identity_class":               lambda n, s, o: do_default_e(n, s, o),
-    "aliases":                      lambda n, s, o: do_aliases(n, s, o),
-    "revoked":                      lambda n, s, o: do_default_e(n, s, o),
-    "x_mitre_shortname":            lambda n, s, o: do_s_p_l(n, s, DCTERMS.alternative, o),
-    "definition_type":              lambda n, s, o: do_default_e(n, s, o),
-    "definition":                   lambda n, s, o: do_default_e(n, s, o),
-    "tactic_refs":                  lambda n, s, o: do_tactic_refs(n, s, o),
-    "labels":                       lambda n, s, o: do_default_e(n, s, o),
-    "x_mitre_aliases":              lambda n, s, o: do_default_e(n, s, o)
+xer = {
+    "external_references":          lambda n, g, s, o: do_external_references(n, g, s, o),
+    "x_mitre_permissions_required": lambda n, g, s, o: do_x_mitre_permissions_required(n, g, s, o),
+    "created_by_ref":               lambda n, g, s, o: do_created_by_ref(n, g, s, o),
+    "x_mitre_platforms":            lambda n, g, s, o: do_x_mitre_platforms(n, g, s, o),
+    "object_marking_refs":          lambda n, g, s, o: do_object_marking_refs(n, g, s, o),
+    "x_mitre_version":              lambda n, g, s, o: do_default_e(n, g, s, o),
+    "x_mitre_data_sources":         lambda n, g, s, o: do_default_e(n, g, s, o),
+    "x_mitre_detection":            lambda n, g, s, o: do_default_e(n, g, s, o),
+    "x_mitre_contributors":         lambda n, g, s, o: do_default_e(n, g, s, o),
+    "x_mitre_effective_permissions":lambda n, g, s, o: do_default_e(n, g, s, o),
+    "x_mitre_system_requirements":  lambda n, g, s, o: do_default_e(n, g, s, o),
+    "x_mitre_remote_support":       lambda n, g, s, o: do_default_e(n, g, s, o),
+    "x_mitre_network_requirements": lambda n, g, s, o: do_default_e(n, g, s, o),
+    "x_mitre_defense_bypassed":     lambda n, g, s, o: do_default_e(n, g, s, o),
+    "source_ref":                   lambda n, g, s, o: do_default_e(n, g, s, o),
+    'relationship_type':            lambda n, g, s, o: do_default_e(n, g, s, o),
+    "target_ref":                   lambda n, g, s, o: do_default_e(n, g, s, o),
+    "identity_class":               lambda n, g, s, o: do_default_e(n, g, s, o),
+    "revoked":                      lambda n, g, s, o: do_default_e(n, g, s, o),
+    "definition_type":              lambda n, g, s, o: do_default_e(n, g, s, o),
+    "definition":                   lambda n, g, s, o: do_default_e(n, g, s, o),
+    "labels":                       lambda n, g, s, o: do_default_e(n, g, s, o),
+    "x_mitre_aliases":              lambda n, g, s, o: do_default_e(n, g, s, o),
+    "name":                         lambda n, g, s, o: do_s_p_l(n, g, s, DCTERMS.title, o),
+    "description":                  lambda n, g, s, o: do_s_p_l(n, g, s, DCTERMS.description, o),
+    "kill_chain_phases":            lambda n, g, s, o: do_kill_chain_phase(n, g, s, o),
+    "modified":                     lambda n, g, s, o: do_s_p_lt(n, g, s, DCTERMS.modifed, o, XSD.dateTime),
+    "created":                      lambda n, g, s, o: do_s_p_lt(n, g, s, DCTERMS.created, o, XSD.dateTime),
+    "aliases":                      lambda n, g, s, o: do_aliases(n, g, s, o),
+    "x_mitre_shortname":            lambda n, g, s, o: do_s_p_l(n, g, s, DCTERMS.alternative, o),
+    "tactic_refs":                  lambda n, g, s, o: do_tactic_refs(n, g, s, o),
 }
+    
 
 ##### OBJECTS ######
 
-def do_default_o(n, o):
-    g = Graph()
+
+def do_default_o(n, g, o):
     if 'id' in o:
         s = n[o['id']]
         if 'type' in o:
+            missed[o['type']] = missed.get(o['type'], 0) + 1
             g.add( (s, RDF.type, n[o['type']]) )
         g += process_element(n, s, o)
-    return g
 
-def do_marking_definition(n, o):
-    g = Graph()
+def do_marking_definition(n, g, o):
+    """
+    {
+      "type": "marking-definition",
+      "id": "marking-definition--fa42a846-8d90-4e51-bc29-71d5b4802168",
+      "created_by_ref": "identity--c78cb6e5-0c4b-4611-8297-d1b8b55e40b5",
+      "created": "2017-06-01T00:00:00Z",
+      "definition_type": "statement",
+      "definition": {
+        "statement": "Copyright 2017, The MITRE Corporation"
+      }
+    }
+    """
     if 'id' in o:
         s = n[o['id']]
         if 'type' in o:
             g.add( (s, RDF.type, DCTERMS.RightsStatement) )
         g += process_element(n, s, o)
-    return g
 
-def do_identity(n, o):
-    g = Graph()
+def do_identity(n, g, o):
     if 'id' in o:
         s = n[o['id']]
         if 'type' in o:
             g.add( (s, RDF.type, DCTERMS.Agent) )
         g += process_element(n, s, o, exclusion=['id', 'type', 'identity_class'])
-    return g
 
-def do_relationship(n, o):
-    g = Graph()
+def do_relationship(n, g, o):
     sub = n[o['source_ref']]
     if o['relationship_type'] == 'mitigates':
         prd = CTI.mitigates
@@ -205,21 +247,35 @@ def do_relationship(n, o):
     g.add ( (statementId, DCTERMS.identifier, Literal(o['id'])) )
     for i in o['object_marking_refs']:
         g.add ( (statementId, DCTERMS.rights, n[i]) )
-    return g
 
 xo = {
-    'attack-pattern':     lambda n, o: do_default_o(n, o),
-    'course-of-action':   lambda n, o: do_default_o(n, o),
-    'identity':           lambda n, o: do_identity(n, o),
-    'intrusion-set':      lambda n, o: do_default_o(n, o),
-    'malware':            lambda n, o: do_default_o(n, o),
-    'marking-definition': lambda n, o: do_marking_definition(n, o),
-    'relationship':       lambda n, o: do_relationship(n, o),
-    'tool':               lambda n, o: do_default_o(n, o),
-    'x-mitre-matrix':     lambda n, o: do_default_o(n, o),
-    'x-mitre-tactic':     lambda n, o: do_default_o(n, o),
+    'attack-pattern':     lambda n, g, o: do_default_o(n, g, o),
+    'course-of-action':   lambda n, g, o: do_default_o(n, g, o),
+    'identity':           lambda n, g, o: do_identity(n, g, o),
+    'intrusion-set':      lambda n, g, o: do_default_o(n, g, o),
+    'malware':            lambda n, g, o: do_default_o(n, g, o),
+    'marking-definition': lambda n, g, o: do_marking_definition(n, g, o),
+    'relationship':       lambda n, g, o: do_relationship(n, g, o),
+    'tool':               lambda n, g, o: do_default_o(n, g, o),
+    'x-mitre-matrix':     lambda n, g, o: do_default_o(n, g, o),
+    'x-mitre-tactic':     lambda n, g, o: do_default_o(n, g, o),
 }
 
+def process_element(n, s, o, exclusion=['id', 'type']):
+    g = Graph()
+    for k in o.keys():
+        if not k in exclusion:
+            if k in xer:
+                xer[k](n, g, s, o[k])
+            else:
+                print("WARNING: Don't know how to process data element '{}'".format(k))
+    return g
+
+def process_objects(n, g, objs):
+    for ao in objs:
+        # print(ao['type'])
+        xo[ao['type']](n, g, ao)
+    
 #####
     
 def parse():
@@ -229,9 +285,9 @@ def parse():
     #print(doc["type"], doc["spec_version"])
     if doc["type"] == "bundle" and doc["spec_version"] == "2.0" and 'objects' in doc:
         #print(len(doc['objects']))
-        for ao in doc['objects']:
-            # print(ao['type'])
-            g = g + xo[ao['type']](n, ao)
+        process_objects(n, g, doc['objects'])
+    print(missed)
+    print(missed_e)
     g.bind('dcterms', DCTERMS)
     g.bind('dc', DC)
     g.bind('core', CORE)
@@ -243,5 +299,6 @@ def parse():
     #print(g.serialize(format='turtle').decode('utf-8'))
     with open('cache/enterprise-attack.ttl', 'w') as f:
         f.write(g.serialize(format='turtle', encoding='utf-8').decode('utf-8'))
+        
 parse()
 
